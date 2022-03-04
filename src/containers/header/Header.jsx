49ae1f83cef59e86391b7463966ea6837b5f1586
Geogsoc <elis.porter80@gmail.com>
2022-03-04 14:24:00 +0000

    added brands

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import "./header.css";
-
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 export default function Header() {
   return (
-    <div className="gpt3__header dection__padding" id="home">
-      <div className="gpt3__deader-content">
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let's Build Something Amazing with GPT-3 OpenAI{" "}
         </h1>
-        <div className="gtp3__header-content__input">
+        <p>
+          Let’s Build Something amazing with GPT-3 OpenAI Yet bed any for
+          travelling assistance indulgence unpleasing. Not thoughts all exercise
+          blessing. Indulgence way everything joy alteration boisterous the
+          attachment. Party we years to order allow asked of. Your Email Address
+          Get Started 1.6k+ 1,600 people requested access a visit in last 24
+          hours
+        </p>
+        <div className="gpt3__header-content__input">
           <input type="email" placeholder="Your email Address" />
           <button type="button">Get Started</button>
         </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        {" "}
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
