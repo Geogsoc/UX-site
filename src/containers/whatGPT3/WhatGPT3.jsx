286a7db2451a23db62d8f499755641c95114df9d
Geogsoc <elis.porter80@gmail.com>
2022-03-07 12:01:26 +0000

    finish whatgpt3 jsx

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import "./whatGPT3.css";
+import Feature from "../../components/feature/Feature";
 
 export default function WhatGPT3() {
-  return <div>whatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 }
